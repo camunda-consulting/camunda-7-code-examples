682683127862b99648337c1b0aed53af93e19cfc
Rafael Cordones <rafael.cordones@plexiti.com>
2013-04-12 11:53:19 +0200

    Fix bug introduced while scoping task list to the "OrderConfirmation" process definition.

--- order-confirmation-rules/src/main/java/org/camunda/bpm/demo/orderconfirmation/bean/TaskList.java
+++ order-confirmation-rules/src/main/java/org/camunda/bpm/demo/orderconfirmation/bean/TaskList.java
@@ -32,9 +32,9 @@ public class TaskList implements Serializable {
 
   public List<Task> getList() {
     if (currentUser != null && currentUser.length() > 0) {
-      return taskService.createTaskQuery().taskDefinitionKey("OrderConfirmation").taskAssignee(currentUser).list();
+      return taskService.createTaskQuery().processDefinitionKey("OrderConfirmation").taskAssignee(currentUser).list();
     } else {
-      return taskService.createTaskQuery().taskDefinitionKey("OrderConfirmation").list();
+      return taskService.createTaskQuery().processDefinitionKey("OrderConfirmation").list();
     }
   }
 
