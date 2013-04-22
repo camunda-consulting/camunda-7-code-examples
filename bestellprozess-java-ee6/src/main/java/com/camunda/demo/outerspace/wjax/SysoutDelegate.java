3739251e3a09005a6dd6060358404a8ffb4ff053
Bernd Ruecker <bernd.ruecker@camunda.com>
2013-04-22 09:54:17 +0200

    small fixes

--- bestellprozess-java-ee6/src/main/java/com/camunda/demo/outerspace/wjax/SysoutDelegate.java
+++ bestellprozess-java-ee6/src/main/java/com/camunda/demo/outerspace/wjax/SysoutDelegate.java
@@ -4,20 +4,15 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
 
-
 public class SysoutDelegate implements JavaDelegate {
 
-  @Override
-  public void execute(DelegateExecution execution) throws Exception {
-    System.out.println("I am here: " + 
-    ((ExecutionEntity)execution).getActivityId());
-    
-    Thread.sleep(30*1000);
-    
-    System.out.println("finished");
-//    
-//    System.out.println( " ORDER ####### " + 
-//            execution.getVariable("orderId"));
-  }
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		System.out.println("I am here: "
+				+ ((ExecutionEntity) execution).getActivityId());
+
+		System.out
+				.println(" ORDER ####### " + execution.getVariable("orderId"));
+	}
 
 }
--- bestellprozess-java-ee6/src/main/java/com/camunda/demo/outerspace/wjax/controller/ArticleController.java
+++ bestellprozess-java-ee6/src/main/java/com/camunda/demo/outerspace/wjax/controller/ArticleController.java
@@ -15,21 +15,22 @@ import com.camunda.demo.outerspace.wjax.service.DemoService;
 
 @ApplicationScoped
 public class ArticleController {
-  
+
   @Inject
   private DemoService demoService;
-  
+
   @PersistenceContext
   private EntityManager entityManager;
-  
+
   @PostConstruct
   public void createDemoArticle() {
     demoService.createDemoData();
   }
 
+  @SuppressWarnings("unchecked")
   @Produces
   @Named
   public List<Article> articles() {
-    return (List<Article>)entityManager.createQuery("select a from Article a").getResultList();    
+    return (List<Article>) entityManager.createQuery("select a from Article a").getResultList();
   }
 }
