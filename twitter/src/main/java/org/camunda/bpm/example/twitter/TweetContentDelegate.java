7a02fc8af1ad60d26e37491d5780cf1299f9d190
Bernd Ruecker <bernd.ruecker@camunda.com>
2013-06-25 18:35:40 +0200

    upgraded to latest twitter4j

--- twitter/src/main/java/org/camunda/bpm/example/twitter/TweetContentDelegate.java
+++ twitter/src/main/java/org/camunda/bpm/example/twitter/TweetContentDelegate.java
@@ -5,7 +5,7 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
-import twitter4j.http.AccessToken;
+import twitter4j.auth.AccessToken;
 
 /**
  * Publish content on Twitter. It really goes live! Watch out http://twitter.com/#!/camunda_demo for your postings.
@@ -16,7 +16,10 @@ public class TweetContentDelegate implements JavaDelegate {
     String content = (String) execution.getVariable("content");
       
     AccessToken accessToken = new AccessToken("220324559-8hWDVUXMSOaAnmtNNwBuNuhGJ6hOGwNdWHqhdOsU", "sGBZafB7saWYMwnPQGjjwU9Ggr0IJYkmPdyAFU5PI");
-    Twitter twitter = new TwitterFactory().getOAuthAuthorizedInstance("HW62M0Rxtt39CbgdgP1og", "TJHNqolmPD6aHPtX8ec5Xp5zgIJcsMBTkwMpGCqdGuk", accessToken);
+    Twitter twitter = new TwitterFactory().getInstance();
+    twitter.setOAuthConsumer("HW62M0Rxtt39CbgdgP1og", "TJHNqolmPD6aHPtX8ec5Xp5zgIJcsMBTkwMpGCqdGuk");
+    twitter.setOAuthAccessToken(accessToken);    
+
     twitter.updateStatus(content);
   }
 
