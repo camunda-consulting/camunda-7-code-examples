00cdf254e58957ed4f59ee7af4606634aa9fdec4
Bernd Ruecker <bernd.ruecker@camunda.com>
2013-08-08 19:42:01 +0200

    small improvements on code/layout

--- bestellprozess-java-ee6/src/main/java/com/camunda/demo/outerspace/wjax/service/OrderService.java
+++ bestellprozess-java-ee6/src/main/java/com/camunda/demo/outerspace/wjax/service/OrderService.java
@@ -12,6 +12,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.cdi.BusinessProcess;
 import org.camunda.bpm.engine.delegate.BpmnError;
 
 import com.camunda.demo.outerspace.wjax.entity.Order;
@@ -21,6 +22,8 @@ import com.camunda.demo.outerspace.wjax.entity.OrderItem;
 @Named
 public class OrderService {
   
+  private static final String VAR_NAME_orderId = "orderId";
+
   @PersistenceContext
   private EntityManager entityManager;
   
@@ -29,32 +32,32 @@ public class OrderService {
   
   public long placeOrder(Order order) {
     entityManager.persist(order);
-    
+    // process variable:
     Map<String, Object> variables = new HashMap<String, Object>();
-    variables.put("orderId", order.getId());
-    // This could be an alternative:
+    variables.put(VAR_NAME_orderId, order.getId());
+    // OR businessKey (alternative!)
     String businessKey = String.valueOf(order.getId());
     
     runtimeService.startProcessInstanceByKey("wjax2012-bestellprozess", businessKey, variables);
     return order.getId();
   }
   
-  public void persist(Order o) {
-    entityManager.persist(o);
-  }
-
-//  @Inject
-//  private BusinessProcess businessProcess;
-
-  @Inject @Named
-  private Map<String, Object> processVariables;
+  @Inject
+  private BusinessProcess businessProcess;
 
   @Produces 
   @Named
   public Order order() {
-    return entityManager.find(Order.class, processVariables.get("orderId"));
+    return entityManager.find(Order.class, businessProcess.getVariable(VAR_NAME_orderId));
   }  
   
+//  @Inject @Named
+//  private Map<String, Object> processVariables;
+  
+  public void persist(Order o) {
+    entityManager.persist(o);
+  }
+
   @Inject
   private StockService stockService;
   
