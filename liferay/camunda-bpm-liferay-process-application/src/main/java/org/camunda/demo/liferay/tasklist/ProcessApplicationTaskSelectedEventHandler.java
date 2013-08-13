8053f2455ce4ed927991867dd01b1aa13dfa7e82
Bernd Ruecker <bernd.ruecker@camunda.com>
2013-08-13 12:12:19 +0200

    some more cleanup

--- liferay/camunda-bpm-liferay-process-application/src/main/java/org/camunda/demo/liferay/tasklist/ProcessApplicationTaskSelectedEventHandler.java
+++ liferay/camunda-bpm-liferay-process-application/src/main/java/org/camunda/demo/liferay/tasklist/ProcessApplicationTaskSelectedEventHandler.java
@@ -1,34 +1,15 @@
 package org.camunda.demo.liferay.tasklist;
 
-import javax.faces.context.ExternalContext;
+import javax.enterprise.context.Conversation;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.portlet.Event;
-import javax.portlet.PortletSession;
 import javax.portlet.faces.event.EventNavigationResult;
 
-import org.camunda.bpm.BpmPlatform;
-import org.camunda.bpm.ProcessApplicationService;
-import org.camunda.bpm.ProcessEngineService;
-import org.camunda.bpm.application.ProcessApplicationDeploymentInfo;
-import org.camunda.bpm.application.ProcessApplicationInfo;
-import org.camunda.bpm.engine.FormService;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.TaskService;
-import org.camunda.bpm.engine.repository.ProcessDefinition;
+import org.camunda.bpm.engine.cdi.BusinessProcess;
 import org.camunda.bpm.engine.task.Task;
-
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.model.Layout;
-import com.liferay.portal.model.LayoutTypePortlet;
-import com.liferay.portal.model.Portlet;
-import com.liferay.portal.model.PortletConstants;
-import com.liferay.portal.service.LayoutLocalServiceUtil;
-import com.liferay.portal.theme.ThemeDisplay;
-import com.liferay.portal.util.PortalUtil;
 
 public class ProcessApplicationTaskSelectedEventHandler extends CdiEnabledBridgeEventHandler {
 
@@ -37,25 +18,22 @@ public class ProcessApplicationTaskSelectedEventHandler extends CdiEnabledBridge
 
   @Inject
   private ProcessEngine processEngine;
-
+  
   protected EventNavigationResult handleEventWithCdi(FacesContext facesContext, Event event) {
     EventNavigationResult eventNavigationResult = null;
     String eventQName = event.getQName().toString();
 
-    if (eventQName.equals("{http://camunda.org/events}ipc.taskSelected")) {
-      
-      // check if the task is part of this PA - otherwise ignore!
-      
+    if (eventQName.equals("{http://camunda.org/events}ipc.taskSelected")) {     
+      // TODO: check if the task is part of this PA - otherwise ignore!      
       String taskId = (String) event.getValue();
-
+      
       Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
       if (task == null) {
         return null;
       }
       
-      // set to backing bean
-      new SelectedTaskBean().setTask(task);      
-
+      // set to backing bean (via PortletContext - we do not have any other scope available here at this moment)
+      new SelectedTaskBean().setTask(task);
     }
     return eventNavigationResult;
 
