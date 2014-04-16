993acd666e3ece5af8c7745d9dc40255a387f5c0
Bernd Ruecker <bernd.ruecker@camunda.com>
2014-04-16 17:08:23 +0200

    adjusted twitter credentials

--- showcases/twitter/src/main/java/org/camunda/bpm/example/twitter/TweetContentDelegate.java
+++ showcases/twitter/src/main/java/org/camunda/bpm/example/twitter/TweetContentDelegate.java
@@ -18,9 +18,9 @@ public class TweetContentDelegate implements JavaDelegate {
   public void execute(DelegateExecution execution) throws Exception {      
     String content = (String) execution.getVariable("content");
       
-    AccessToken accessToken = new AccessToken("220324559-8hWDVUXMSOaAnmtNNwBuNuhGJ6hOGwNdWHqhdOsU", "sGBZafB7saWYMwnPQGjjwU9Ggr0IJYkmPdyAFU5PI");
+    AccessToken accessToken = new AccessToken("220324559-jet1dkzhSOeDWdaclI48z5txJRFLCnLOK45qStvo", "B28Ze8VDucBdiE38aVQqTxOyPc7eHunxBVv7XgGim4say");
     Twitter twitter = new TwitterFactory().getInstance();
-    twitter.setOAuthConsumer("HW62M0Rxtt39CbgdgP1og", "TJHNqolmPD6aHPtX8ec5Xp5zgIJcsMBTkwMpGCqdGuk");
+    twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk", "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
     twitter.setOAuthAccessToken(accessToken);    
 
     twitter.updateStatus(content);
