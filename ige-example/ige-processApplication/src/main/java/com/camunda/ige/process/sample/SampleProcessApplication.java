87556bd1abffffa4a18aefbbdbf01feaf86b32e2
ingorichtsmeier <ingo.richtsmeier@camunda.com>
2014-06-18 13:09:49 +0200

    a process app with two sample processes to test the cockpit

---
+++ ige-example/ige-processApplication/src/main/java/com/camunda/ige/process/sample/SampleProcessApplication.java
@@ -0,0 +1,9 @@
+package com.camunda.ige.process.sample;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("IGE sample processes")
+public class SampleProcessApplication extends ServletProcessApplication {
+
+}
