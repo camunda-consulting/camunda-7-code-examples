e22ee0e74e5b829bdd877c89ec820bcf1e21b66f
Bernd Ruecker <bernd.ruecker@camunda.com>
2014-07-28 15:24:55 +0200

    use camunda bpm assert in Twitter Showcase

--- showcases/twitter/src/test/java/org/camunda/bpm/example/twitter/ProcessTestCase.java
+++ showcases/twitter/src/test/java/org/camunda/bpm/example/twitter/ProcessTestCase.java
@@ -1,23 +1,17 @@
 package org.camunda.bpm.example.twitter;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.processEngine;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.camunda.bpm.engine.history.HistoricActivityInstance;
-import org.camunda.bpm.engine.history.HistoricProcessInstance;
-import org.camunda.bpm.engine.impl.test.ProcessEngineAssert;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.matchers.JUnitMatchers;
 
 public class ProcessTestCase {
 
@@ -33,50 +27,27 @@ public class ProcessTestCase {
     Map<String, Object> variables = new HashMap<String, Object>();
     variables.put("content", "We will never see this content on Twitter");
     variables.put("email", "bernd.ruecker@camunda.com");
+    ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey("TwitterDemoProcess", variables);
 
-    ProcessInstance processInstance = processEngineRule.getRuntimeService()
-              .startProcessInstanceByKey("TwitterDemoProcess", variables);
-
-    String id = processInstance.getId();
-    System.out.println("Started process instance id " + id);
-
-    Assert.assertThat(processEngineRule.getRuntimeService().getActiveActivityIds(id), JUnitMatchers.hasItem("user_task_review_tweet"));
-
-    List<HistoricActivityInstance> historyActivities = processEngineRule.getProcessEngine().getHistoryService().createHistoricActivityInstanceQuery() //
-            .processInstanceId(processInstance.getId()) //
-            .finished() //
-            .orderByActivityId().asc() //
-            .list();
-
-    assertEquals(1, historyActivities.size());
-    assertEquals("start_event_new_tweet", historyActivities.get(0).getActivityId());
-
+    System.out.println("Started process instance " + processInstance);
+    
+    // check userTask
+    // using camunda-bpm-assert
+    assertThat(processInstance).isStarted() //
+    	.isWaitingAt("user_task_review_tweet") //
+    	.task() //
+    	.isAssignedTo("demo");
+    
+    // or using the Java API
     Task task = processEngineRule.getTaskService().createTaskQuery().taskAssignee("demo").singleResult();
     variables.put("approved", Boolean.FALSE);
     variables.put("comments", "No, we will not publish this on Twitter");
-    processEngineRule.getTaskService().complete(task.getId(), variables);
+    processEngine().getTaskService().complete(task.getId(), variables);
 
-    ProcessEngineAssert.assertProcessEnded(processEngineRule.getProcessEngine(), id);
-
-    HistoricProcessInstance historicProcessInstance = processEngineRule.getHistoryService().createHistoricProcessInstanceQuery().processInstanceId(id)
-            .singleResult();
-    assertNotNull(historicProcessInstance);
-
-    System.out.println("Finished, took " + historicProcessInstance.getDurationInMillis() + " millis");
-
-    historyActivities = processEngineRule.getProcessEngine().getHistoryService().createHistoricActivityInstanceQuery() //
-            .processInstanceId(processInstance.getId()) //
-            .finished() //
-            .orderByActivityId().asc() // we order this way as it may happen that the time stamp is eactly the same in an in memory test case
-            .list();
-
-    assertEquals(6, historyActivities.size());
-    assertEquals("end_event_tweet_handled", historyActivities.get(0).getActivityId());
-    assertEquals("gateway_approved", historyActivities.get(1).getActivityId());
-    assertEquals("gateway_join", historyActivities.get(2).getActivityId());
-    assertEquals("service_task_send_rejection_notification", historyActivities.get(3).getActivityId());
-    assertEquals("start_event_new_tweet", historyActivities.get(4).getActivityId());
-    assertEquals("user_task_review_tweet", historyActivities.get(5).getActivityId());
+    // assert that all activities where passed
+    assertThat(processInstance) //
+    	.isEnded() //
+    	.hasPassed("end_event_tweet_handled", "gateway_approved", "gateway_join", "service_task_send_rejection_notification", "start_event_new_tweet", "user_task_review_tweet");
 
   }
 
