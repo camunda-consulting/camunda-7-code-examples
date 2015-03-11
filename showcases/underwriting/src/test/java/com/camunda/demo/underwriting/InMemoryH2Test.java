9d153066f74defa2a034e047c5dd22ac66554774
Bernd Ruecker <bernd.ruecker@camunda.com>
2015-03-11 16:09:26 +0100

    added drools decision table to find underwriter

--- showcases/underwriting/src/test/java/com/camunda/demo/underwriting/InMemoryH2Test.java
+++ showcases/underwriting/src/test/java/com/camunda/demo/underwriting/InMemoryH2Test.java
@@ -26,6 +26,7 @@ import org.camunda.bpm.engine.variable.Variables;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mvel2.ast.Contains;
 
 import com.camunda.demo.underwriting.Constants;
 import com.camunda.demo.underwriting.domain.Application;
@@ -70,20 +71,22 @@ public class InMemoryH2Test {
   @Deployment(resources = "underwriting-case.cmmn")
   public void testUnderwritingCase() {
     ProcessEngine processEngine = processEngine();
-    
+
     // HashMap containing variables to be added to case instance
     VariableMap variables = Variables.createVariables().putValue("application", new Application());
-    
+
     // create the case instance
     CaseInstance caseInstance = processEngine.getCaseService().createCaseInstanceByKey("underwriting", variables);
 
-    // load tasks via Query API and assert that we have one task created (corresponding to one active Activity)
+    // load tasks via Query API and assert that we have one task created
+    // (corresponding to one active Activity)
     List<Task> tasks = processEngine.getTaskService().createTaskQuery().list();
     assertEquals(1, tasks.size());
     // use camunda assertions to check we created the right task:
-    assertThat(tasks.get(0)).hasDefinitionKey("PI_humanTaskDecide"); 
+    assertThat(tasks.get(0)).hasDefinitionKey("PI_humanTaskDecide");
 
-    // load all so called "executions" of a Case Instance, basically corresponding to Activities in our case
+    // load all so called "executions" of a Case Instance, basically
+    // corresponding to Activities in our case
     List<CaseExecution> caseExecutions = processEngine.getCaseService().createCaseExecutionQuery().list();
     for (CaseExecution caseExecution : caseExecutions) {
       if (caseExecution.isAvailable()) {
@@ -98,14 +101,13 @@ public class InMemoryH2Test {
 
     // complete task - changes the state of the case instance
     processEngine.getTaskService().complete(tasks.get(0).getId());
-    
-    // we could check completed activities now: 
-    processEngine.getHistoryService().createHistoricCaseActivityInstanceQuery()
-      .caseInstanceId(caseInstance.getId()).list();
+
+    // we could check completed activities now:
+    processEngine.getHistoryService().createHistoricCaseActivityInstanceQuery().caseInstanceId(caseInstance.getId()).list();
 
     if (true)
       return;
-    
+
     System.out.println("####################");
     caseExecutions = processEngine.getCaseService().createCaseExecutionQuery().caseInstanceId(caseInstance.getId()).list();
     for (CaseExecution otherCaseExecution : caseExecutions) {
@@ -134,46 +136,92 @@ public class InMemoryH2Test {
   }
 
   @Test
-  @Deployment(resources = {"underwriting-case.cmmn", "underwriting-process.bpmn"})
+  @Deployment(resources = { "underwriting-case.cmmn", "underwriting-process.bpmn" })
   public void testUnderwritingProcessIncludingCase() {
     VariableMap variables = Variables.createVariables().putValue("application", new Application());
 
     ProcessInstance pi = processEngine().getRuntimeService().startProcessInstanceByMessage(Constants.MSG_START_ELECTRONIC_APPLICATION, variables);
-    
+
     assertThat(pi).isWaitingAtExactly("CallActivityCaseUnderwriting");
     CaseInstance caseInstance = processEngine().getCaseService().createCaseInstanceQuery() //
-//          .variableValueEquals(Constants.VAR_NAME_PROCESS_INSTANCE_ID, pi.getProcessInstanceId()) //
-          .singleResult();
-    
+        // .variableValueEquals(Constants.VAR_NAME_PROCESS_INSTANCE_ID,
+        // pi.getProcessInstanceId()) //
+        .singleResult();
+
     assertNotNull(caseInstance);
-//    assertEquals(pi.getProcessInstanceId(), caseInstance.get());
+    // assertEquals(pi.getProcessInstanceId(), caseInstance.get());
 
     Task task = processEngine().getTaskService().createTaskQuery().caseInstanceId(caseInstance.getId()).singleResult();
     assertNotNull(task);
     assertEquals("PI_humanTaskDecide", task.getTaskDefinitionKey());
 
-    // on <= 7.2.x set variable in a seperate statement because of https://app.camunda.com/jira/browse/CAM-3261
-    // processEngine().getCaseService().setVariable(caseInstance.getId(), "approved", Boolean.TRUE);
+    // on <= 7.2.x set variable in a seperate statement because of
+    // https://app.camunda.com/jira/browse/CAM-3261
+    // processEngine().getCaseService().setVariable(caseInstance.getId(),
+    // "approved", Boolean.TRUE);
     processEngine().getTaskService().complete(task.getId(), Variables.createVariables().putValue("approved", Boolean.TRUE));
 
     assertThat(pi).isEnded().hasPassed("endEventApplicationProcessed");
-    
-    System.out.println(
-        processEngine().getHistoryService().createHistoricCaseActivityInstanceQuery().caseInstanceId(caseInstance.getId()).list() 
-        );
-    
+
+    System.out.println(processEngine().getHistoryService().createHistoricCaseActivityInstanceQuery().caseInstanceId(caseInstance.getId()).list());
+
     // assert case instance
-    assertEquals(1,
-        processEngine().getHistoryService().createHistoricCaseActivityInstanceQuery().caseInstanceId(caseInstance.getId()) //
-          .caseActivityId("PI_milestoneApproved").count());
-    
+    assertEquals(1, processEngine().getHistoryService().createHistoricCaseActivityInstanceQuery().caseInstanceId(caseInstance.getId()) //
+        .caseActivityId("PI_milestoneApproved").count());
+
     HistoricCaseInstance historicCaseInstance = processEngine().getHistoryService().createHistoricCaseInstanceQuery().caseInstanceId(caseInstance.getId())
         .singleResult();
     assertTrue(historicCaseInstance.isClosed());
-//    assertTrue(historicCaseInstance.isCompleted());
+    // assertTrue(historicCaseInstance.isCompleted());
 
-//    processEngine().getCaseService().closeCaseInstance(caseInstance.getId());
+    // processEngine().getCaseService().closeCaseInstance(caseInstance.getId());
     assertEquals(0, processEngine().getCaseService().createCaseInstanceQuery().caseInstanceId(caseInstance.getId()).count());
+
+  }
+
+  @Test
+  @Deployment(resources = { "underwriting-case.cmmn", "underwriting-process.bpmn" })
+  public void testAssignmentRules() {
+    Application application = new Application();
+    application.setAge(50);
+    application.setRisk("niedrig");
+    application.setType("gesetzlich");
+
+    ProcessInstance pi = processEngine().getRuntimeService().startProcessInstanceByMessage( //
+        Constants.MSG_START_ELECTRONIC_APPLICATION, //
+        Variables.createVariables().putValue("application", application));
+
+    Task task = processEngine().getTaskService().createTaskQuery().singleResult();
+
+    System.out.println(processEngine().getRuntimeService().getVariable(pi.getId(), "capableUnderwriters"));
+    System.out.println(processEngine().getTaskService().getIdentityLinksForTask(task.getId()).toString());
+
+    
+//    assertThat(processEngine().getTaskService().getIdentityLinksForTask(task.getId()), //
+//        hasItem(hasProperty(""), is("")));
+//    
+//    "[demo, mary, peter]"
+    processEngine().getTaskService().complete(task.getId(), Variables.createVariables().putValue("approved", true));
+  }
+  
+  @Test
+  @Deployment(resources = { "underwriting-case.cmmn", "underwriting-process.bpmn" })
+  public void testAssignmentRules2() {
+    Application application = new Application();
+    application.setAge(70);
+    application.setRisk("niedrig");
+    application.setType("Chefarztgarantie");
+
+    ProcessInstance pi = processEngine().getRuntimeService().startProcessInstanceByMessage( //
+        Constants.MSG_START_ELECTRONIC_APPLICATION, //
+        Variables.createVariables().putValue("application", application));
+
+    Task task = processEngine().getTaskService().createTaskQuery().singleResult();
+    System.out.println(processEngine().getRuntimeService().getVariable(pi.getId(), "capableUnderwriters"));
+    System.out.println(processEngine().getTaskService().getIdentityLinksForTask(task.getId()).toString());
+//    assertEquals("[demo]", processEngine().getTaskService().getIdentityLinksForTask(task.getId()).toString());
+    
+    processEngine().getTaskService().complete(task.getId(), Variables.createVariables().putValue("approved", true));
 
   }
 }
