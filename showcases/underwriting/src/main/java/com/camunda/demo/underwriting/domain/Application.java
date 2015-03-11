534b2b1920673a28b820dd5baa31d4066e959489
Bernd Ruecker <bernd.ruecker@camunda.com>
2015-03-11 14:44:56 +0100

    some bugfixes

--- showcases/underwriting/src/main/java/com/camunda/demo/underwriting/domain/Application.java
+++ showcases/underwriting/src/main/java/com/camunda/demo/underwriting/domain/Application.java
@@ -1,5 +1,7 @@
 package com.camunda.demo.underwriting.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Application {
@@ -9,7 +11,13 @@ public class Application {
   private String customerId;
   private String customerName;
   
+  private int age;
+  private String risk;
+  private String type;
+  
   private boolean smoker;
+  
+  private List<String> clerkRequirements = new ArrayList<String>();  
   
   public Application() {
     id = UUID.randomUUID().toString();
@@ -46,6 +54,38 @@ public class Application {
 
   public void setSmoker(boolean smoker) {
     this.smoker = smoker;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getRisk() {
+    return risk;
+  }
+
+  public void setRisk(String risk) {
+    this.risk = risk;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public List<String> getClerkRequirements() {
+    return clerkRequirements;
+  }
+
+  public void addClerkRequirement(String clerkRequirement) {
+    this.clerkRequirements.add(clerkRequirement);
   }
 
 }
