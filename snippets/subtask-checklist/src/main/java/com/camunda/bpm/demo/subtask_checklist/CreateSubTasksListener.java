aaef31448878e421a01b4ed21266df422167e679
Bernd Ruecker <bernd.ruecker@camunda.com>
2015-03-20 13:46:19 +0100

    removed unused json object, start with array

--- snippets/subtask-checklist/src/main/java/com/camunda/bpm/demo/subtask_checklist/CreateSubTasksListener.java
+++ snippets/subtask-checklist/src/main/java/com/camunda/bpm/demo/subtask_checklist/CreateSubTasksListener.java
@@ -7,15 +7,8 @@ import org.camunda.bpm.engine.delegate.TaskListener;
 import org.camunda.bpm.engine.impl.util.json.JSONArray;
 import org.camunda.bpm.engine.impl.util.json.JSONObject;
 import org.camunda.bpm.engine.task.Task;
-import org.camunda.bpm.model.bpmn.Query;
-import org.camunda.bpm.model.bpmn.impl.instance.camunda.CamundaPropertiesImpl;
-import org.camunda.bpm.model.bpmn.instance.ExtensionElements;
 import org.camunda.bpm.model.bpmn.instance.camunda.CamundaProperties;
 import org.camunda.bpm.model.bpmn.instance.camunda.CamundaProperty;
-import org.camunda.bpm.model.xml.instance.ModelElementInstance;
-import org.camunda.spin.json.SpinJsonNode;
-
-import static org.camunda.spin.Spin.*;
 
 public class CreateSubTasksListener implements TaskListener {
 
@@ -27,10 +20,11 @@ public class CreateSubTasksListener implements TaskListener {
     Collection<CamundaProperty> properties = camundaProperties.getCamundaProperties();
     for (CamundaProperty camundaProperty : properties) {
       if ("subtaskConfiguration".equals( camundaProperty.getCamundaName() )) {
-        SpinJsonNode json = JSON(camundaProperty.getCamundaValue());
-        for (SpinJsonNode subtask : json.prop("subtasks").elements()) {
-          String name = subtask.prop("taskName").stringValue();
-          String formKey = subtask.prop("taskFormKey").stringValue();
+        JSONArray subtasks = new JSONArray(camundaProperty.getCamundaValue());
+        for (int i = 0; i < subtasks.length(); i++) {
+          JSONObject subtask = subtasks.getJSONObject(i);
+          String name = subtask.getString("taskName");
+          String formKey = subtask.getString("taskFormKey");
           
           Task newTask = delegateTask.getProcessEngineServices().getTaskService()
             .newTask();
@@ -38,20 +32,14 @@ public class CreateSubTasksListener implements TaskListener {
           newTask.setName(name);
           newTask.setDescription(formKey);
           delegateTask.getProcessEngineServices().getTaskService().saveTask(newTask);
-          delegateTask.getProcessEngineServices().getTaskService().setVariableLocal(newTask.getId(), "parentTaskId", delegateTask.getId());
         }
       }
     }
     
-    Collection<CamundaProperties> elementsByType = delegateTask.getProcessEngineServices().getRepositoryService().getBpmnModelInstance(
-        delegateTask.getProcessDefinitionId()).getModelElementsByType(CamundaProperties.class);
-    
-    String st = elementsByType.iterator().next().getCamundaProperties().iterator().next().getCamundaValue();
-    System.out.println(st);
-    System.out.println(
-      JSON(st)
-      .prop("subtasks")
-    );
+    // you can read properties (like formKey) from "the outside" as will via public API:
+//    Collection<CamundaProperties> elementsByType = delegateTask.getProcessEngineServices().getRepositoryService().getBpmnModelInstance(
+//        delegateTask.getProcessDefinitionId()).getModelElementsByType(CamundaProperties.class);    
+//    String st = elementsByType.iterator().next().getCamundaProperties().iterator().next().getCamundaValue();
   }
   
   
