9a68fda5ca39de7a903396bde4cafc5e399ba6e1
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2015-03-24 16:54:20 +0100

    finish lab 3 part 2

---
+++ snippets/twitter-qa/src/main/java/com/camunda/training/twitterQa/CreateTweetDelegate.java
@@ -0,0 +1,30 @@
+package com.camunda.training.twitterQa;
+
+import java.util.logging.Logger;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class CreateTweetDelegate implements JavaDelegate {
+
+  private static final Logger LOGGER = Logger.getLogger(CreateTweetDelegate.class.getName());
+
+  @Override
+  public void execute(DelegateExecution execution) throws Exception {
+    String content = "I did it! Cheers The Trainer";
+
+    LOGGER.info("Publishing tweet: " + content);
+
+    AccessToken accessToken = new AccessToken("220324559-jet1dkzhSOeDWdaclI48z5txJRFLCnLOK45qStvo", "B28Ze8VDucBdiE38aVQqTxOyPc7eHunxBVv7XgGim4say");
+    Twitter twitter = new TwitterFactory().getInstance();
+    twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk", "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
+    twitter.setOAuthAccessToken(accessToken);
+ 
+    twitter.updateStatus(content);
+  }
+
+}
