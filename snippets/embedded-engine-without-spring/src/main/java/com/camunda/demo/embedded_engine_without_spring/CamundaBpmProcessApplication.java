cfdcc9167cb3f1a1ba26e62834080a530339d4d6
Falko Menge <falko.menge@camunda.com>
2015-08-13 16:38:54 +0200

    Add snippet for embedded engine without Spring

---
+++ snippets/embedded-engine-without-spring/src/main/java/com/camunda/demo/embedded_engine_without_spring/CamundaBpmProcessApplication.java
@@ -0,0 +1,28 @@
+package com.camunda.demo.embedded_engine_without_spring;
+
+import org.camunda.bpm.application.PostDeploy;
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.engine.ProcessEngine;
+
+/**
+ * Process Application exposing this application's resources the process engine. 
+ */
+@ProcessApplication
+public class CamundaBpmProcessApplication extends ServletProcessApplication {
+
+  private static final String PROCESS_DEFINITION_KEY = "embedded-engine-without-spring";
+
+  /**
+   * In a @PostDeploy Hook you can interact with the process engine and access 
+   * the processes the application has deployed. 
+   */
+  @PostDeploy
+  public void onDeploymentFinished(ProcessEngine processEngine) {
+
+    // start an initial process instance
+    processEngine.getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+
+  }
+
+}
---
+++ snippets/embedded-engine-without-spring/src/test/java/com/camunda/demo/embedded_engine_without_spring/nonarquillian/InMemoryH2Test.java
@@ -0,0 +1,44 @@
+package com.camunda.demo.embedded_engine_without_spring.nonarquillian;
+
+import org.apache.ibatis.logging.LogFactory;
+import org.camunda.bpm.engine.impl.util.LogUtil;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.Deployment;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.junit.Assert.*;
+
+/**
+ * Test case starting an in-memory database-backed Process Engine.
+ */
+public class InMemoryH2Test {
+
+  @Rule
+  public ProcessEngineRule rule = new ProcessEngineRule();
+
+  private static final String PROCESS_DEFINITION_KEY = "embedded-engine-without-spring";
+
+  // enable more detailed logging
+  static {
+//    LogUtil.readJavaUtilLoggingConfigFromClasspath(); // process engine
+//    LogFactory.useJdkLogging(); // MyBatis
+  }
+  
+  @Before
+  public void setup() {
+    init(rule.getProcessEngine());
+  }
+
+  /**
+   * Just tests if the process definition is deployable.
+   */
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testParsingAndDeployment() {
+    // nothing is done here, as we just want to check for exceptions during deployment
+  }
+
+}
