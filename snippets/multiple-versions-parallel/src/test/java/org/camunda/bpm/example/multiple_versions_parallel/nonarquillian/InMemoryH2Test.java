036098b1a8fe055700c1a65dace2ed709535041a
Falko Menge <falko.menge@camunda.com>
2015-10-08 14:21:51 +0200

    Add user tasks and example code for selecting the latest version

--- snippets/multiple-versions-parallel/src/test/java/org/camunda/bpm/example/multiple_versions_parallel/nonarquillian/InMemoryH2Test.java
+++ snippets/multiple-versions-parallel/src/test/java/org/camunda/bpm/example/multiple_versions_parallel/nonarquillian/InMemoryH2Test.java
@@ -1,10 +1,12 @@
 package org.camunda.bpm.example.multiple_versions_parallel.nonarquillian;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
 import org.camunda.bpm.engine.impl.util.LogUtil;
+import org.camunda.bpm.engine.repository.ProcessDefinition;
 import org.camunda.bpm.engine.test.ProcessEngineTestCase;
 import org.camunda.bpm.engine.test.Deployment;
 
@@ -20,15 +22,23 @@ public class InMemoryH2Test extends ProcessEngineTestCase {
     LogUtil.readJavaUtilLoggingConfigFromClasspath();
   }
 
-  /**
-   * Just tests if the process definition is deployable.
-   * @throws IOException 
-   */
   @Deployment(resources = "process.bpmn")
   public void testParsingAndDeployment() throws IOException {
     Properties version = new Properties();
     version.load(this.getClass().getResourceAsStream("/version.properties"));
     runtimeService.startProcessInstanceByKey(PROCESS_DEFINITION_KEY + version.getProperty("maven.version.major") + "." + version.getProperty("maven.version.minor"));
+  }
+
+  @Deployment(resources = { "process.bpmn", "process-v23.bpmn" })
+  public void testStartLatestVerison() {
+    List<ProcessDefinition> processDefinitions = repositoryService.createProcessDefinitionQuery()
+      .processDefinitionKeyLike(PROCESS_DEFINITION_KEY + "%")
+      .orderByProcessDefinitionName()
+      .desc()
+      .latestVersion()
+      .listPage(0, 1);
+    
+    assertEquals(PROCESS_DEFINITION_KEY + "42.0", processDefinitions.get(0).getKey());
   }
 
 }
