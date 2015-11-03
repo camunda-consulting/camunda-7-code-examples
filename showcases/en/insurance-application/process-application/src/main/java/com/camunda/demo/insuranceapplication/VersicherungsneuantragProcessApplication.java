11208fad7a57d590769f4fde05ceea287b2f7e56
Bernd Ruecker <bernd.ruecker@camunda.com>
2015-11-03 08:13:13 +0100

    First version of translated demo

---
+++ showcases/en/insurance-application/process-application/src/main/java/com/camunda/demo/insuranceapplication/VersicherungsneuantragProcessApplication.java
@@ -0,0 +1,9 @@
+package com.camunda.demo.insuranceapplication;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication
+public class VersicherungsneuantragProcessApplication extends ServletProcessApplication {
+
+}
---
+++ showcases/en/insurance-application/process-application/src/main/java/com/camunda/demo/insuranceapplication/model/Car.java
@@ -0,0 +1,20 @@
+package com.camunda.demo.insuranceapplication.model;
+
+public class Car {
+  
+  private String manufacturer;
+  private String type;
+  
+  public String getManufacturer() {
+    return manufacturer;
+  }
+  public void setManufacturer(String manufacturer) {
+    this.manufacturer = manufacturer;
+  }
+  public String getType() {
+    return type;
+  }
+  public void setType(String type) {
+    this.type = type;
+  }
+}
