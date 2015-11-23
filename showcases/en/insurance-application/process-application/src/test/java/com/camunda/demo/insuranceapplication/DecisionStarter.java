345e7f9116c45bf433d86aed6e862a58dafed92b
Bernd Ruecker <bernd.ruecker@camunda.com>
2015-11-23 13:19:28 +0100

    updated to alpha3

--- showcases/en/insurance-application/process-application/src/test/java/com/camunda/demo/insuranceapplication/DecisionStarter.java
+++ showcases/en/insurance-application/process-application/src/test/java/com/camunda/demo/insuranceapplication/DecisionStarter.java
@@ -2,11 +2,10 @@ package com.camunda.demo.insuranceapplication;
 
 import java.util.Map;
 
-import org.camunda.bpm.BpmPlatform;
 import org.camunda.bpm.dmn.engine.DmnDecision;
-import org.camunda.bpm.dmn.engine.DmnDecisionResult;
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
 import org.camunda.bpm.dmn.engine.DmnEngine;
-import org.camunda.bpm.dmn.engine.impl.DmnEngineConfigurationImpl;
+import org.camunda.bpm.dmn.engine.impl.DefaultDmnEngineConfiguration;
 import org.camunda.bpm.engine.variable.Variables;
 
 public class DecisionStarter {
@@ -15,12 +14,11 @@ public class DecisionStarter {
     Map<String, Object> variables = Variables.createVariables();
     variables.put("application", DemoData.createNewApplication(25, "BMW", "318i"));
 
-    DmnEngine dmnEngine = new DmnEngineConfigurationImpl().buildEngine();
+    DmnEngine dmnEngine = new DefaultDmnEngineConfiguration().buildEngine();
     
-    DmnDecision decision = dmnEngine.parseDecision("RiskAssessment.dmn");   
-    DmnDecisionResult result = dmnEngine.evaluate(decision, variables);
+    DmnDecision decision = dmnEngine.parseDecision("riskAssessment", DecisionStarter.class.getResourceAsStream("/RiskAssessment.dmn"));   
+    DmnDecisionTableResult result = dmnEngine.evaluateDecisionTable(decision, variables);
         
     System.out.println(result.get(0).get("risk"));
-    System.out.println(result.get(1).get("risk"));
   }
 }
