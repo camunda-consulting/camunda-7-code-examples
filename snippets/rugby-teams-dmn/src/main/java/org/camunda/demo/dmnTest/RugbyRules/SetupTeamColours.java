3d7f368bd2dd6be955a0b45c73d8374f6659d87c
Niall <niall.p.deehan@gmail.com>
2015-12-04 14:04:02 +0100

    Added simple DMN example using Rugby teams.

---
+++ snippets/rugby-teams-dmn/src/main/java/org/camunda/demo/dmnTest/RugbyRules/SetupTeamColours.java
@@ -0,0 +1,42 @@
+package org.camunda.demo.dmnTest.RugbyRules;
+
+import java.util.Vector;
+import java.util.logging.Logger;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.Variables.SerializationDataFormats;
+import org.camunda.bpm.engine.variable.value.ObjectValue;
+
+public class SetupTeamColours implements ExecutionListener {
+
+	 private final Logger LOGGY = Logger.getLogger(LoggerDelegate.class.getName());
+		
+	@Override
+	public void notify(DelegateExecution arg0) throws Exception {
+		
+		LOGGY.info("Started Setting up colours");
+		
+		Vector<String> teamColours = new Vector<String>();
+		teamColours.add("Green");
+		teamColours.add("Red");
+		teamColours.add("White");
+		teamColours.add("Black");
+		teamColours.add("Yellow");
+		teamColours.add("Green");
+		teamColours.add("Dark Blue");
+		teamColours.add("Light Blue");
+		
+		ObjectValue teamColoursSerialized =
+				Variables.objectValue(teamColours).serializationDataFormat(SerializationDataFormats.JSON).create();
+
+		
+		arg0.setVariable("teamColours", teamColoursSerialized);
+		
+		arg0.getProcessEngineServices().getHistoryService().createHistoricActivityInstanceQuery().p
+		
+		
+	}
+
+}
