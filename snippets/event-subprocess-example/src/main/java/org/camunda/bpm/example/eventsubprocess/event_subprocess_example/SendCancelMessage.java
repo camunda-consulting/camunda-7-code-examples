5bd1ffeb01a3db434bd8aabfec77538f43387a8e
Niall <niall.p.deehan@gmail.com>
2015-12-15 10:08:55 +0100

    Example of message event sub-processes
    This is a simple snippet which demonstrates how interupting and
    non-interupting message start events work within an event sub-process.

---
+++ snippets/event-subprocess-example/src/main/java/org/camunda/bpm/example/eventsubprocess/event_subprocess_example/SendCancelMessage.java
@@ -0,0 +1,41 @@
+package org.camunda.bpm.example.eventsubprocess.event_subprocess_example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class SendCancelMessage implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		String orderBusKey = (String)execution.getVariable("orderBusKey");
+		String messageType = (String)execution.getVariable("messageType");
+		String orderedItem = (String)execution.getVariable("orderedItem");
+		String reason = (String)execution.getVariable("reason");
+		
+		String messageFrom = execution.getProcessEngineServices().getIdentityService().getCurrentAuthentication().getUserId();
+		
+		//the variables to pass via message
+		Map<String, Object> vars = new HashMap<String, Object>();
+		vars.put("reason", reason);
+		vars.put("messageFrom", messageFrom);
+		
+		if(orderedItem == null){
+			
+			execution.getProcessEngineServices().getRuntimeService().correlateMessage(messageType, orderBusKey, vars);
+			
+		}else{
+			//correlation keys
+			Map<String, Object> correlationKeys = new HashMap<String, Object>();
+			correlationKeys.put("orderedItem", orderedItem);
+			
+			execution.getProcessEngineServices().getRuntimeService().correlateMessage(messageType, orderBusKey, correlationKeys, vars);
+			
+		}
+
+	}
+
+}
