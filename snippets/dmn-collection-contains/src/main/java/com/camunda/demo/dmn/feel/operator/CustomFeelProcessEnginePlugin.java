9650b68c9e70d3ff4e7c75aa558afe3f5dd79e82
Bernd Ruecker <bernd.ruecker@camunda.com>
2016-02-25 12:15:03 +0100

    adjusted to 7.4.0 api

--- snippets/dmn-collection-contains/src/main/java/com/camunda/demo/dmn/feel/operator/CustomFeelProcessEnginePlugin.java
+++ snippets/dmn-collection-contains/src/main/java/com/camunda/demo/dmn/feel/operator/CustomFeelProcessEnginePlugin.java
@@ -1,66 +1,20 @@
 package com.camunda.demo.dmn.feel.operator;
 
-import java.lang.reflect.Field;
-
-import org.camunda.bpm.dmn.engine.DmnEngine;
-import org.camunda.bpm.dmn.engine.DmnEngineConfiguration;
 import org.camunda.bpm.dmn.engine.impl.DefaultDmnEngineConfiguration;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
-import org.camunda.bpm.engine.impl.dmn.configuration.ProcessEngineDmnEngineConfiguration;
-import org.camunda.bpm.engine.impl.el.ExpressionManager;
-import org.camunda.bpm.engine.impl.history.parser.HistoryDecisionTableListener;
-import org.camunda.bpm.engine.impl.scripting.engine.ScriptingEngines;
 
 public class CustomFeelProcessEnginePlugin implements ProcessEnginePlugin {
 
   public void preInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
-    // cannot do this here because historyLevel (and maybe others) are not yet initialized
+      DefaultDmnEngineConfiguration dmnEngineConfiguration = (DefaultDmnEngineConfiguration) processEngineConfiguration.getDmnEngineConfiguration();
+      dmnEngineConfiguration.setFeelEngineFactory(new CustomFeelEngineFactory());      
   }
 
   public void postInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
-    // so do it here
-    try {
-      
-//      ScriptingEngines scriptingEngines = processEngineConfiguration.getScriptingEngines();
-//      HistoryDecisionTableListener historyDecisionTableListener = new HistoryDecisionTableListener(processEngineConfiguration.getDmnHistoryEventProducer(),
-//          processEngineConfiguration.getHistoryLevel());
-//      ExpressionManager expressionManager = processEngineConfiguration.getExpressionManager();
-//
-//      ProcessEngineDmnEngineConfiguration dmnEngineConfiguration = new ProcessEngineDmnEngineConfiguration(scriptingEngines, historyDecisionTableListener,
-//          expressionManager);
-
-      DefaultDmnEngineConfiguration dmnEngineConfiguration = (DefaultDmnEngineConfiguration) processEngineConfiguration.getDmnEngineConfiguration();
-      dmnEngineConfiguration.setFeelEngineFactory(new CustomFeelEngineFactory());      
-      DmnEngine dmnEngine = dmnEngineConfiguration.buildEngine();
-
-      Field dmnEngineConfigurationField = getField(processEngineConfiguration.getClass(), "dmnEngineConfiguration");
-      dmnEngineConfigurationField.setAccessible(true);
-      dmnEngineConfigurationField.set(processEngineConfiguration, dmnEngineConfiguration);
-
-      Field dmnEngineField = getField(processEngineConfiguration.getClass(), "dmnEngine");
-      dmnEngineField.setAccessible(true);
-      dmnEngineField.set(processEngineConfiguration, dmnEngine);
-
-    } catch (Exception ex) {
-      throw new RuntimeException("Could not configure DMN Engine", ex);
-    }    
   }
 
   public void postProcessEngineBuild(ProcessEngine processEngine) {
-  }
-
-  public static Field getField(Class clazz, String fieldName) throws NoSuchFieldException {
-    try {
-      return clazz.getDeclaredField(fieldName);
-    } catch (NoSuchFieldException e) {
-      Class superClass = clazz.getSuperclass();
-      if (superClass == null) {
-        throw e;
-      } else {
-        return getField(superClass, fieldName);
-      }
-    }
   }
 }
