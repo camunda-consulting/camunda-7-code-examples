f8960a54bca3cac18291d0e1beca5ba7afbb8633
Bernd Ruecker <bernd.ruecker@camunda.com>
2016-06-14 18:22:34 +0200

    fixed plugin configuration

--- snippets/dmn-collection-contains/src/main/java/com/camunda/demo/dmn/feel/operator/CustomFeelProcessEnginePlugin.java
+++ snippets/dmn-collection-contains/src/main/java/com/camunda/demo/dmn/feel/operator/CustomFeelProcessEnginePlugin.java
@@ -1,5 +1,6 @@
 package com.camunda.demo.dmn.feel.operator;
 
+import org.camunda.bpm.dmn.engine.DmnEngine;
 import org.camunda.bpm.dmn.engine.impl.DefaultDmnEngineConfiguration;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
@@ -7,14 +8,23 @@ import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
 
 public class CustomFeelProcessEnginePlugin implements ProcessEnginePlugin {
 
-  public void preInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
-      DefaultDmnEngineConfiguration dmnEngineConfiguration = (DefaultDmnEngineConfiguration) processEngineConfiguration.getDmnEngineConfiguration();
-      dmnEngineConfiguration.setFeelEngineFactory(new CustomFeelEngineFactory());      
-  }
+	public void preInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
+		// cannot do this here because historyLevel (and maybe others) are not
+		// yet initialized
+	}
 
-  public void postInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
-  }
+	public void postInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
+		// so do it here
+		DefaultDmnEngineConfiguration dmnEngineConfiguration = (DefaultDmnEngineConfiguration) processEngineConfiguration.getDmnEngineConfiguration();
+		dmnEngineConfiguration.setFeelEngineFactory(new CustomFeelEngineFactory());
+		DmnEngine dmnEngine = dmnEngineConfiguration.buildEngine();
 
-  public void postProcessEngineBuild(ProcessEngine processEngine) {
-  }
+		processEngineConfiguration.setDmnEngineConfiguration(dmnEngineConfiguration);
+		processEngineConfiguration.setDmnEngine(dmnEngine);
+
+	}
+
+	public void postProcessEngineBuild(ProcessEngine processEngine) {
+	}
+
 }
