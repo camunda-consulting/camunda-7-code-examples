90d84ac76bb973dc877a910595cb3eff843b8c35
Falko Menge <falko.menge@camunda.com>
2016-08-26 10:42:57 +0200

    feat(executify): executify Process and Case Tasks

--- snippets/executify/src/main/java/org/camunda/bpm/demo/executify/CmmnExecutifier.java
+++ snippets/executify/src/main/java/org/camunda/bpm/demo/executify/CmmnExecutifier.java
@@ -4,13 +4,17 @@ import static org.camunda.bpm.demo.executify.CmmnModelInstanceHelper.*;
 
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.List;
 
-import org.camunda.bpm.model.cmmn.instance.ConditionExpression;
 import org.camunda.bpm.model.cmmn.Cmmn;
 import org.camunda.bpm.model.cmmn.CmmnModelInstance;
-import org.camunda.bpm.model.cmmn.instance.CmmnElement;
+import org.camunda.bpm.model.cmmn.instance.CaseTask;
+import org.camunda.bpm.model.cmmn.instance.ExtensionElements;
 import org.camunda.bpm.model.cmmn.instance.IfPart;
+import org.camunda.bpm.model.cmmn.instance.ProcessTask;
 import org.camunda.bpm.model.cmmn.instance.Sentry;
+import org.camunda.bpm.model.cmmn.instance.Task;
+import org.camunda.bpm.model.cmmn.instance.camunda.CamundaIn;
 
 public class CmmnExecutifier {
 
@@ -35,6 +39,42 @@ public class CmmnExecutifier {
   public void executify(CmmnModelInstance modelInstance) {
     this.modelInstance = modelInstance;
     addMissingIfPartConditions();
+    executifyProcessAndCaseTasks();
+  }
+
+  private void executifyProcessAndCaseTasks() {
+    Collection<Task> callActivities = modelInstance.getModelElementsByType(Task.class);
+    interationOverAllCallActivities : for (Task callActivity : callActivities) {
+      if (callActivity instanceof ProcessTask || callActivity instanceof CaseTask) {
+        // add process or case if missing
+        String name = callActivity.getName();
+        if (callActivity instanceof ProcessTask && isEmpty(((ProcessTask) callActivity).getProcess())) {
+          ((ProcessTask) callActivity).setProcess("TODO");
+        } else if (callActivity instanceof CaseTask && isEmpty(((CaseTask) callActivity).getCase())) {
+          ((CaseTask) callActivity).setCase("TODO");
+        }
+        // add business key if missing
+        ExtensionElements extensionElements = callActivity.getExtensionElements();
+        if (extensionElements != null) {
+          List<CamundaIn> camundaIns = extensionElements
+              .getElementsQuery()
+              .filterByType(CamundaIn.class)
+              .list();
+          for (CamundaIn camundaIn : camundaIns) {
+            if (!isEmpty(camundaIn.getCamundaBusinessKey())) {
+              continue interationOverAllCallActivities;
+            }
+          }
+          CamundaIn camundaIn = extensionElements.addExtensionElement(CamundaIn.class);
+          camundaIn.setCamundaBusinessKey("#{execution.processBusinessKey}");
+        } else {
+          extensionElements = createElement(callActivity, ExtensionElements.class);
+          CamundaIn camundaIn = extensionElements.addExtensionElement(CamundaIn.class);
+          camundaIn.setCamundaBusinessKey("#{execution.processBusinessKey}");
+          callActivity.setExtensionElements(extensionElements);
+        }
+      }
+    }
   }
 
   private void addMissingIfPartConditions() {
