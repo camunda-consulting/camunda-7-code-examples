94b07b2f1f931a0712fe89ddc241cc462172f7f0
Felix Mueller <felix.mueller@camunda.com>
2016-11-01 14:34:19 +0100

    Add download buttons

--- snippets/bpmnjs-custom-modeler/app/app.js
+++ snippets/bpmnjs-custom-modeler/app/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var $ = require('jquery');
 // inlined diagram; load it from somewhere else if you like
 var pizzaDiagram = require('../resources/pizza-collaboration.bpmn');
 
@@ -34,8 +34,28 @@ modeler.importXML(pizzaDiagram, function(err) {
   modeler.get('canvas').zoom('fit-viewport');
 
   modeler.addCustomElements(customElements);
-});
 
+  function saveSVG(event) {
+    modeler.saveSVG(function(err,svg) {
+      $(event.target).attr({
+        'href': 'data:application/bpmn20-xml;charset=UTF-8,' + svg,
+        'download': 'name.svg'
+      });
+    });
+  }
+  function saveDiagram(event) {
+    modeler.saveXML({ format: true }, function(err, xml) {
+        $(event.target).attr({
+          'href': 'data:application/bpmn20-xml;charset=UTF-8,' + xml,
+          'download': 'name.bpmn'
+        });
+    });
+  }
+  var downloadLink = $('#js-download-diagram');
+  var downloadSvgLink = $('#js-download-svg');
+  downloadLink.on('click',saveDiagram);
+  downloadSvgLink.on('click',saveSVG);
+});
 
 // expose bpmnjs to window for debugging purposes
 window.bpmnjs = modeler;
