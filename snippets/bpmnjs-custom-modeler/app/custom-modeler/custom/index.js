75afe7f9b8d375ddb036526f40050c1e9c1616a8
Felix Mueller <felix.mueller@camunda.com>
2016-11-17 14:57:00 +0100

    Custom modeler - remove complex gateway from gateway morph popup menu

--- snippets/bpmnjs-custom-modeler/app/custom-modeler/custom/index.js
+++ snippets/bpmnjs-custom-modeler/app/custom-modeler/custom/index.js
@@ -1,9 +1,10 @@
 module.exports = {
-  __init__: [ 'customRenderer', 'paletteProvider', 'customRules', 'customUpdater', 'contextPadProvider' ],
+  __init__: [ 'customRenderer', 'paletteProvider', 'customRules', 'customUpdater', 'contextPadProvider', 'replaceMenuProvider' ],
   elementFactory: [ 'type', require('./CustomElementFactory') ],
   customRenderer: [ 'type', require('./CustomRenderer') ],
   paletteProvider: [ 'type', require('./CustomPalette') ],
   customRules: [ 'type', require('./CustomRules') ],
   customUpdater: [ 'type', require('./CustomUpdater') ],
+  replaceMenuProvider: [ 'type', require('./CustomReplaceMenuProvider') ],
   contextPadProvider: [ 'type', require('./CustomContextPadProvider') ]
 };
