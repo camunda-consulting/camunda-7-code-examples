765064a6effb188094a851dc97b5da6c1edfd501
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2019-05-17 09:38:20 -0600

    looping worker

--- snippets/python-external-worker/PythonWorker.py
+++ snippets/python-external-worker/PythonWorker.py
@@ -2,59 +2,64 @@ import requests
 import json
 import time
 
-#Define API Endpoint
 url = 'http://localhost:8080/engine-rest/external-task/fetchAndLock'
 
-# Define Json
-task= {"workerId":"Nele",
-  "maxTasks":2,
+fetchAndLockPayload = {"workerId":"myExampleWorker",
+  "maxTasks":1,
   "usePriority":"true",
   "topics":
-      [{"topicName": "work",
-      "lockDuration": 10000
+      [{"topicName": "credit-charge",
+      "lockDuration": 30000
       }]
 }
 
-
-# API call (Fetch and Lock)
 try:
-    res = requests.post(url, json=task)
-    print(res.status_code)
+    while True: 
 
-#Get Body Text
-    
-    body = res.text    
-    print(body)    
+        # Fetch and Lock
+        try:
+            res = requests.post(url, json=fetchAndLockPayload)
+            print('Fetch and lock status code: ', res.status_code)
+
+        #Get Body Text
+            body = res.text    
+            print('Fetch and lock response: ', body)    
+                    
+        except: 
+            print('Engine is down')
+
+        while body == '[]':
+            time.sleep(5)
+            res = requests.post(url, json=fetchAndLockPayload)
+            body = res.text
+            print ('Fetch and lock response: ', body)
             
-except: 
-    print('Engine is down')
+            if body !='[]':
+                break
 
-while body == '[]':
-    print (body)
-    res = requests.post(url, json=task)
-    time.sleep(5)
-    
-    if body !='[]':
-        break
+        # Put you Business Logic here
+        print('Deduct existing credit')
 
-# Put you Business Logic here
+        #Prepare url with task id and the response body
+        response = json.loads(body)
+        taskId = response[0]['id']
+        taskId = str(taskId)
 
-#Prepare url with task id and the response body
-response = json.loads(body)
-x = response[0]['id']
-x = str(x)
+        complete_url = ('http://localhost:8080/'
+        'engine-rest/external-task/'
+        + taskId + '/complete')
 
-complete_url = 'http://localhost:8080/engine-rest/external-task/'+x+'/complete'
+        response = {
+          "workerId": "myExampleWorker",   
+        }
 
-response= {
-  "workerId": "Nele",   
- }
+        # API call (Complete)
+        try:
+            complete = requests.post(complete_url, json=response)
+            print('complete status code: ', complete.status_code)
 
-# API call (Complete)
-try:
-    complete = requests.post(complete_url, json=response)
-    print(complete.status_code)
+        except: 
+            print('failed')
 
-except: 
-    print('fail')
-
+except KeyboardInterrupt:
+    pass
