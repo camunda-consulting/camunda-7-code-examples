106246748eaffe2d78589c6c52346b0c94842c7b
Stefan Wiese <stefan.wiese@camunda.com>
2019-07-15 17:21:26 +0200

    added lombok

--- snippets/generate-model-from-json/src/main/java/com/camunda/demo/domain/EventTypeConfiguration.java
+++ snippets/generate-model-from-json/src/main/java/com/camunda/demo/domain/EventTypeConfiguration.java
@@ -1,9 +1,14 @@
-package com.camunda.demo;
+package com.camunda.demo.domain;
 
 import java.util.List;
+import lombok.Data;
+
 
 public class EventTypeConfiguration {
+
+    @Data
     private String eventType;
 
+    @Data
     private List<UserTaskData> configuration;
 }
--- snippets/generate-model-from-json/src/main/java/com/camunda/demo/domain/UserTaskData.java
+++ snippets/generate-model-from-json/src/main/java/com/camunda/demo/domain/UserTaskData.java
@@ -1,12 +1,21 @@
 package com.camunda.demo.domain;
 
 import java.util.List;
+import lombok.Data;
+
 
 public class UserTaskData {
     
+    @Data
     private String id;
+    
+    @Data
     private String name;
+
+    @Data
     private String formkey;
+
+    @Data
     private List<UserTaskData> children;
 
     
