e01931893062ac02ee526e4553c89d9db96e6c57
Benjamin Hoffmann <benjamin.hoffmann@camunda.com>
2019-07-15 17:43:00 +0200

    stub for modelgenerator

--- snippets/generate-model-from-json/src/main/java/com/camunda/demo/rest/GenerateModelController.java
+++ snippets/generate-model-from-json/src/main/java/com/camunda/demo/rest/GenerateModelController.java
@@ -1,18 +1,33 @@
 package com.camunda.demo.rest;
 
 import com.camunda.demo.domain.EventTypeConfiguration;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
 public class GenerateModelController {
+
+	@Autowired
+	RepositoryService repositoryService;
     
     @PostMapping("/model")
-	public String createModel(@RequestParam("eventTypeConfiguration") EventTypeConfiguration configuration) {
-		
-		return null;
+	public String createModel(@RequestBody EventTypeConfiguration configuration) {
+
+		BpmnModelInstance model = ModelGenerator.generateModel(configuration);
+
+    	repositoryService.createDeployment()
+						.name(configuration.getEventType())
+						.addModelInstance(configuration.getEventType(), model)
+						.source("deployment-generator")
+						.enableDuplicateFiltering(true);
+
+
+		return configuration.toString();
 	}
 }
