86253f5be1f22d71359891c218faa861190acd4e
Falko Menge <falko.menge@camunda.com>
2019-11-08 02:01:48 +0100

    Add asynchronous Service Task example using a scheduled executor

---
+++ snippets/asynchronous-service-task/src/main/java/com/camunda/bpm/consulting/snippet/asynchronous_service_task/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.camunda.bpm.consulting.snippet.asynchronous_service_task;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "asynchronous-service-task"; // BPMN Process ID
+
+}
