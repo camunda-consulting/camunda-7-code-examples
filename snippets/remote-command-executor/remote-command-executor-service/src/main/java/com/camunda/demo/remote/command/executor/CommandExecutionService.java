cbc13a3cd35470ad6bcc5db841286af3ec3b52cb
Benjamin Hoffmann <benjamin.hoffmann@camunda.com>
2020-01-06 13:00:48 +0100

    Clean up obsolete snippet remote-command-executor

--- snippets/remote-command-executor/remote-command-executor-service/src/main/java/com/camunda/demo/remote/command/executor/CommandExecutionService.java
+++
@@ -1,9 +0,0 @@
-package com.camunda.demo.remote.command.executor;
-
-import org.camunda.bpm.engine.impl.interceptor.Command;
-
-public interface CommandExecutionService {
-
-  <T> T execute(Command<T> command);
-
-}
