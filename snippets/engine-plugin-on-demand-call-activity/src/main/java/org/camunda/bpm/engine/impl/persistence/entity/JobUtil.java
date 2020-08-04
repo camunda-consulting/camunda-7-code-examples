f313c579d79b86ec1593623eb47a1c0bb7bbde9d
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2020-08-04 10:29:25 +0200

    link to it's own repo.

--- snippets/engine-plugin-on-demand-call-activity/src/main/java/org/camunda/bpm/engine/impl/persistence/entity/JobUtil.java
+++
@@ -1,8 +0,0 @@
-package org.camunda.bpm.engine.impl.persistence.entity;
-
-public class JobUtil {
-
-    public static void createIncident(JobEntity jobEntity){
-        jobEntity.createFailedJobIncident();
-    }
-}
