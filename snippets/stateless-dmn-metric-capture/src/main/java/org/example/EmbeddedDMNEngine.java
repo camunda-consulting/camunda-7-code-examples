ab290c041d794691c8f845f72072ffac94c1d90d
Joe Pappas <joe.pappas@me.com>
2021-08-27 14:54:10 -0400

    Updated EmbeddedDMNEngine class

--- snippets/stateless-dmn-metric-capture/src/main/java/org/example/EmbeddedDMNEngine.java
+++ snippets/stateless-dmn-metric-capture/src/main/java/org/example/EmbeddedDMNEngine.java
@@ -14,52 +14,56 @@ public class EmbeddedDMNEngine {
 
     private final static Logger LOGGER = Logger.getLogger(EmbeddedDMNEngine.class.getName());
 
-    public static void main (String[] args) throws Exception {
-        // create dmn engine config
-        DmnEngineConfiguration configuration = DmnEngineConfiguration.createDefaultDmnEngineConfiguration();
+    public static void main (String[] args) {
+        try {
+            // create dmn engine config
+            DmnEngineConfiguration configuration = DmnEngineConfiguration.createDefaultDmnEngineConfiguration();
 
-        // Create metric collector
-        DefaultEngineMetricCollector metricCollector = new DefaultEngineMetricCollector();
+            // Create metric collector
+            DefaultEngineMetricCollector metricCollector = new DefaultEngineMetricCollector();
 
-        // add metric collector to config
-        configuration.setEngineMetricCollector(metricCollector);
+            // add metric collector to config
+            configuration.setEngineMetricCollector(metricCollector);
 
-        // build engine using config
-        DmnEngine dmnEngine = configuration.buildEngine();
+            // build engine using config
+            DmnEngine dmnEngine = configuration.buildEngine();
 
-        // read sample dmn and convert to inputstream
-        File file = new File("./src/main/resources/sample.dmn");
-        InputStream in = new FileInputStream(file);
-        InputStream in2 = new FileInputStream(file);
+            // read sample dmn and convert to inputstream
+            File file = new File("./src/main/resources/sample.dmn");
+            InputStream in = new FileInputStream(file);
+            InputStream in2 = new FileInputStream(file);
 
-        // create inputs to dmns
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("input", "yes"); // or no
-        map.put("output", "");
+            // create inputs to dmns
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("input", "yes"); // or no
+            map.put("output", "");
 
-        // add rules to engine
-        DmnDecision dmnd1 = dmnEngine.parseDecision("Decision_1", in);
-        DmnDecision dmnd2 = dmnEngine.parseDecision("Decision_2", in2);
+            // add rules to engine
+            DmnDecision dmnd1 = dmnEngine.parseDecision("Decision_1", in);
+            DmnDecision dmnd2 = dmnEngine.parseDecision("Decision_2", in2);
 
-        // evaluate decision(s) (repeat as needed)
-        DmnDecisionResult dmndr = dmnEngine.evaluateDecision(dmnd1, map);
-        String result = dmndr.getSingleEntry();
-        LOGGER.info("Result of Decision 1 is "+result);
+            // evaluate decision(s) (repeat as needed)
+            DmnDecisionResult dmndr = dmnEngine.evaluateDecision(dmnd1, map);
+            String result = dmndr.getSingleEntry();
+            LOGGER.info("Result of Decision 1 is " + result);
 
-        dmndr = dmnEngine.evaluateDecision(dmnd1, map);
-        result = dmndr.getSingleEntry();
-        LOGGER.info("Result of Decision 1 is "+result);
+            dmndr = dmnEngine.evaluateDecision(dmnd1, map);
+            result = dmndr.getSingleEntry();
+            LOGGER.info("Result of Decision 1 is " + result);
 
-        dmndr = dmnEngine.evaluateDecision(dmnd2, map);
-        result = dmndr.getSingleEntry();
-        LOGGER.info("Result of Decision 2 is "+result);
+            dmndr = dmnEngine.evaluateDecision(dmnd2, map);
+            result = dmndr.getSingleEntry();
+            LOGGER.info("Result of Decision 2 is " + result);
 
 
-        // get executed DIs
-        long dis = dmnEngine.getConfiguration().getEngineMetricCollector().getExecutedDecisionInstances();
-        LOGGER.info("Executed DIs are "+ dis);
+            // get executed DIs
+            long dis = dmnEngine.getConfiguration().getEngineMetricCollector().getExecutedDecisionInstances();
+            LOGGER.info("Executed DIs are " + dis);
 
-        // at this point the DI data should be sent somewhere for safekeeping
+            // at this point the DI data should be sent somewhere for safekeeping
+        } catch (Exception e) {
+            LOGGER.severe("Error! "+e);
+        }
     }
 }
 
