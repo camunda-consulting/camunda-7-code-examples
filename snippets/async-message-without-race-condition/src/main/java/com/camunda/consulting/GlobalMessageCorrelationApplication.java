ea67f3f807cfa79f35587b6f8204a689e533940d
Jonathan Lukas <jonathan.lukas@camunda.com>
2021-10-05 12:54:53 +0200

    Added a scenario

--- snippets/async-message-without-race-condition/src/main/java/com/camunda/consulting/GlobalMessageCorrelationApplication.java
+++ snippets/async-message-without-race-condition/src/main/java/com/camunda/consulting/GlobalMessageCorrelationApplication.java
@@ -1,16 +1,24 @@
 package com.camunda.consulting;
 
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.joda.time.DateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.camunda.consulting.simulator.SimulationExecutor;
+import com.camunda.consulting.simulator.SimulatorPlugin;
 
 @SpringBootApplication
 @EnableProcessApplication
-public class GlobalMessageCorrelationApplication
-{
-	public static void main(String[] args)
-	{
-		SpringApplication.run(GlobalMessageCorrelationApplication.class, args);
+public class GlobalMessageCorrelationApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(GlobalMessageCorrelationApplication.class, args);
+    SimulationExecutor.execute(DateTime.now().minusMonths(1).toDate(), DateTime.now().toDate());
+  }
 
-	}
+  @Bean
+  public SimulatorPlugin simulatorPlugin() {
+    return new SimulatorPlugin();
+  }
 }
