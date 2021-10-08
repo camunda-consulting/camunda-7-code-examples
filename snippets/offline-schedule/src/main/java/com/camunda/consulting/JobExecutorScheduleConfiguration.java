16d53618ea5df45efe087872e3d560a921c2bdaf
Jonathan Lukas <jonathan.lukas@camunda.com>
2021-10-08 10:07:09 +0200

    Adjusted Configuration to not mix it up

---
+++ snippets/offline-schedule/src/main/java/com/camunda/consulting/JobExecutorScheduleConfiguration.java
@@ -0,0 +1,33 @@
+package com.camunda.consulting;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+public class JobExecutorScheduleConfiguration {
+  private final ProcessEngine processEngine;
+
+  @Autowired
+  public JobExecutorScheduleConfiguration(ProcessEngine processEngine) {
+    this.processEngine = processEngine;
+  }
+
+  @Scheduled(cron = "0 5 10 * * *")
+  public void stopJobExecutor() {
+    getProcessEngineConfigurationImpl(this.processEngine).getJobExecutor().shutdown();
+  }
+
+  @Scheduled(cron = "0 10 10 * * *")
+  public void startJobExecutor() {
+    getProcessEngineConfigurationImpl(this.processEngine).getJobExecutor().start();
+  }
+
+  private static ProcessEngineConfigurationImpl getProcessEngineConfigurationImpl(ProcessEngine processEngine) {
+    return (ProcessEngineConfigurationImpl) processEngine.getProcessEngineConfiguration();
+  }
+}
--- snippets/offline-schedule/src/main/java/com/camunda/consulting/OfflineSchedulePluginConfiguration.java
+++ snippets/offline-schedule/src/main/java/com/camunda/consulting/OfflineSchedulePluginConfiguration.java
@@ -3,12 +3,9 @@ package com.camunda.consulting;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 public class OfflineSchedulePluginConfiguration extends OfflineSchedulePluginProperties {
@@ -21,19 +18,5 @@ public class OfflineSchedulePluginConfiguration extends OfflineSchedulePluginPro
   @Bean
   public Set<JobExecutorBreak> breaks() {
     return this.getBreaks().stream().map(JobExecutorBreakParser::parse).collect(Collectors.toSet());
-  }
-
-  @Scheduled(cron = "0 0 3 * *")
-  public void stopJobExecutor(ProcessEngine processEngine) {
-    getProcessEngineConfigurationImpl(processEngine).getJobExecutor().shutdown();
-  }
-
-  @Scheduled(cron = "0 0 5 * *")
-  public void startJobExecutor(ProcessEngine processEngine) {
-    getProcessEngineConfigurationImpl(processEngine).getJobExecutor().start();
-  }
-
-  private static ProcessEngineConfigurationImpl getProcessEngineConfigurationImpl(ProcessEngine processEngine) {
-    return (ProcessEngineConfigurationImpl) processEngine.getProcessEngineConfiguration();
   }
 }
